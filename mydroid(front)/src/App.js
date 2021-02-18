6d362ad582a8d0d7b8019061cb37e2a2393f741c
sadegh <thesadegh.h@gmail.com>
2021-02-18 13:11:11 +0330

    working on sign up page

--- mydroid(front)/src/App.js
+++ mydroid(front)/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SignIN from './Component/SignInPage';
+import SignUP from './Component/SignUp';
 
 function App() {
   return (
     <div className="appClass">
       <SignIN />
+      <SignUP />
     </div>
   );
 }
---
+++ mydroid(front)/src/Component/SignUp.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+function SignUp() {
+    return (
+        <div>
+            <div className="container">
+                <form>
+                    <input className="my-3" />
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default SignUp
