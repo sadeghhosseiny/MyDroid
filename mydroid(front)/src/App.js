cf4bde148021f90af53203dd43ab6c921fb9b023
sadegh <thesadegh.h@gmail.com>
2021-02-18 17:02:13 +0330

    working on Sign up page

--- mydroid(front)/src/App.js
+++ mydroid(front)/src/App.js
@@ -2,13 +2,22 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SignIN from './Component/SignInPage';
 import SignUP from './Component/SignUp';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="appClass">
-      <SignIN />
-      <SignUP />
-    </div>
+    <Router>
+      <div className="appClass">
+        <Switch>
+          <Route exact path="/">
+            <SignUP />
+          </Route>
+          <Route path="/sign in">
+            <SignIN />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- mydroid(front)/src/Component/SignUp.js
+++ mydroid(front)/src/Component/SignUp.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import styles from './SignUp.module.css';
 
 function SignUp() {
     return (
-        <div>
-            <div className="container">
+        <div className={`${styles.mainContainer}`}>
+            <div className="container d-flex">
                 <form>
+                    <input className="my-3 mx-3" />
                     <input className="my-3" />
                 </form>
             </div>
