64040489baf3570d6e836ca115cedf5fa55e6458
sadegh <thesadegh.h@gmail.com>
2021-03-15 21:44:15 +0330

    change everything and use react-paginate

--- mydroid(front)/src/Component/Games.js
+++ mydroid(front)/src/Component/Games.js
@@ -1,118 +1,69 @@
 import React, { useContext, useEffect, useState } from 'react'
-import under_construction from '../under-construction.jpg';
 import { MyContext } from './Context';
 import styles from './Games.module.css';
+import ReactPaginate from 'react-paginate';
+import axios from 'axios';
 
-
-const renderData = (bestGames) => {
-    return (
-        <ul>
-            {bestGames ? bestGames.map((com, i) => {
-
-                return <li key={"com" + i}>{com.Description}</li>;
-
-            }) : ""}
-
-
-        </ul>
-    )
-}
 
 function Games() {
 
     const { bestGames } = useContext(MyContext);
-    //const tempBestGames = bestGames ? bestGames : "";
-    const [currentPage, setCurrentPage] = useState(1);
-    const [itemsPerPage, setItemsPerPage] = useState(1);
-    const [pageNumberLimit, setPageNumberLimit] = useState(5);
-    const [maxPageNumberLimit, setMaxPageNumberLimit] = useState(5);
-    const [minPageNumberLimit, setMinPageNumberLimit] = useState(0);
 
+    const [offset, setOffset] = useState(0);
     const [data, setData] = useState([]);
+    const [perPage] = useState(1);
+    const [pageCount, setPageCount] = useState(0);
+    //const [currentpage, setCurrentpage] = useState(0);
 
     const getDescription = () => {
-        bestGames ? bestGames.forEach(item => {
-            console.log("Name", item.Name)
-            setData(prevArr => [...prevArr, item.Description])
-        }) : ""
+
+        const data = bestGames ? bestGames : "";
+        //const data = res.data.data;
+        const Slice = data.slice(offset, offset + perPage);
+        const postData = Slice.map(pd =>
+            <p key={pd.ID}>
+                {pd.Description}
+            </p>
+        )
+        setData(postData);
+        setPageCount(Math.ceil(data.length / perPage));
+
     }
 
+    const handlePageClick = (e) => {
+        const selectedPage = e.selected;
+        const Offset = selectedPage * perPage;
+        //setCurrentpage(selectedPage);
+        setOffset(Offset);
+
+    };
 
     useEffect(() => {
         getDescription();
-
-    }, [])
-
-    const handleClick = (event) => {
-        setCurrentPage(Number(event.target.id));
-    }
-
-    const pages = [];
-    for (let i = 1; i <= Math.ceil(data.length / itemsPerPage); i++) {
-        pages.push(i);
-    }
-
-    const renderPageNumber = pages.map((number) => {
-        if (number < maxPageNumberLimit + 1 && number > minPageNumberLimit) {
-
-            return (
-                <li key={number} id={number} className={`${currentPage == number && styles.active} px-2 py-2 ${styles.pageNumbers}`}
-                    onClick={handleClick}>
-                    {number}
-                </li>
-            )
-        }
-        else {
-            return null;
-        }
-    }
-    )
-
-    const handleNextBtn = () => {
-        setCurrentPage(currentPage + 1)
-        if (currentPage + 1 > maxPageNumberLimit) {
-            setMaxPageNumberLimit(maxPageNumberLimit + pageNumberLimit);
-            setMinPageNumberLimit(minPageNumberLimit + pageNumberLimit);
-        }
-    }
-
-    const handlePrevBtn = () => {
-        setCurrentPage(currentPage - 1)
-        if ((currentPage - 1) % pageNumberLimit == 0) {
-            setMaxPageNumberLimit(maxPageNumberLimit - pageNumberLimit);
-            setMinPageNumberLimit(minPageNumberLimit - pageNumberLimit);
-        }
-    }
-
-    const indexOfLastItem = currentPage * itemsPerPage;
-    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
-    const currentItems = bestGames ? bestGames.slice(indexOfFirstItem, indexOfLastItem) : "";
-
+    }, [offset])
 
 
     return (
-        <div className="text-center">
-            {console.log("fffffffffffffff", data.length)}
-            {/* <img src={under_construction} alt="pic" className={` ${ styles.gamePage }`} /> */}
-            {console.log(bestGames)}
-            <div>
-                {renderData(currentItems)}
-            </div>
-            <div className={`d-flex my-2 justify-content-center`}>
-
-                <div>
-                    <button className="btn btn-dark mx-2" onClick={handlePrevBtn}>Previous</button>
-                </div>
-
-                {renderPageNumber}
-
-                <div>
-                    <button className="btn btn-primary mx-2" onClick={handleNextBtn}>Next</button>
-                </div>
-            </div>
+        <div className="Games">
+            {console.log("perPage", perPage)}
+            {console.log("offset", offset)}
+            {console.log("data", data)}
+            {console.log("dataLength", data.length)}
+            {data}
+            <ReactPaginate
+                previousLabel={`prev`}
+                nextLabel={"next"}
+                breakLabel={"..."}
+                breakClassName={"break-me"}
+                pageCount={pageCount}
+                marginPagesDisplayed={1}
+                pageRangeDisplayed={3}
+                onPageChange={handlePageClick}
+                containerClassName={`pagination ${styles.pagination}`}
+                subContainerClassName={"pages pagination"}
+                activeClassName={`active ${styles.active}`} />
         </div>
-
-    )
+    );
 }
 
 export default Games
