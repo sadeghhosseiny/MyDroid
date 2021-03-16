a4d824faf3fdc52c82b1b35497b688963fea3d25
sadegh <thesadegh.h@gmail.com>
2021-03-16 16:06:22 +0330

    add games with all stuf

--- mydroid(front)/src/Component/Games.js
+++ mydroid(front)/src/Component/Games.js
@@ -2,13 +2,11 @@ import React, { useContext, useEffect, useState } from 'react'
 import { MyContext } from './Context';
 import styles from './Games.module.css';
 import ReactPaginate from 'react-paginate';
-import axios from 'axios';
 
 
 function Games() {
 
     const { bestGames } = useContext(MyContext);
-
     const [offset, setOffset] = useState(0);
     const [data, setData] = useState([]);
     const [perPage] = useState(1);
@@ -16,15 +14,31 @@ function Games() {
     //const [currentpage, setCurrentpage] = useState(0);
 
     const getDescription = () => {
-
+        const baseUrl = "http://localhost:8080";
         const data = bestGames ? bestGames : [];
         //const data = res.data.data;
         const Slice = data.slice(offset, offset + perPage);
         console.log("SLICE", Slice);
         const postData = Slice.map(pd =>
-            <p key={pd.ID}>
-                {pd.Description}
-            </p>
+            <div key={pd.ID}>
+                <div className={`${styles.imageDiv}`}>
+
+                    <img className={`my-4 ${styles.image}`} src={baseUrl + pd.ImageUrl} alt="photo Of App" />
+                </div>
+                <div className="d-flex justify-content-center">
+                    <p className={`font-weight-bold ${styles.gameName}`}>{pd.Name}</p>
+                </div>
+                <div className={`d-flex justify-content-center ${styles.pubDiv}`}>
+                    <p className={`text-dark font-weight-bold`}>From</p>
+                </div>
+                <div className={`d-flex justify-content-center`}>
+
+                    <p className={`${styles.pub}`}>{pd.Publisher}</p>
+                </div>
+                <div className="d-flex justify-content-center">
+                    <p className={`text-secondary font-weight-bold ${styles.description}`}>{pd.Description}</p>
+                </div>
+            </div>
         )
         setData(postData);
         setPageCount(Math.ceil(data.length / perPage));
@@ -44,18 +58,11 @@ function Games() {
 
     }, [offset, bestGames])
 
-    // const el = null;
-    // if (offset == 0) {
-    //     previousLabel = null
-    // }
 
     return (
         <div className="Games">
             {console.log('gam', bestGames)}
-            {/* {console.log("perPage", perPage)}
-            {console.log("offset", offset)}
-            {console.log("data", data)}
-            {console.log("dataLength", data.length)} */}
+
             <div className="d-flex justify-content-center">
                 {data}
             </div>
